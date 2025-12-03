7312b361aeccd8ecf7a8c8ce40deebc83e66cf31
mjerta <info@mpdev.nl>
2025-12-03 23:06:21 +0100

    Get the logif for at least splitting the rangers. Now I still need to figure to puzzle and its aloright to solve it

---
+++ 2025/day2/Gift-Shop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./example-puzzle-input.txt";
+        string inputLine = "";
+
+        if (File.Exists(filePath))
+        {
+            inputLine = File.ReadAllText(filePath);
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+
+
+        Console.WriteLine(inputLine);
+        CrackManager crackManager = new CrackManager();
+        crackManager.splitTheString(inputLine);
+        crackManager.getRanges()
+          .ForEach(range => Console.WriteLine(range));
+
+    }
+}
+
+
+class CrackManager
+{
+    private List<string> range;
+
+    public CrackManager()
+    {
+        this.range = new List<string>();
+    }
+
+
+    public void splitTheString(string input)
+    {
+        this.range = input.Split(",").ToList();
+    }
+
+    public List<string> getRanges() {
+      return this.range;
+    }
+}
